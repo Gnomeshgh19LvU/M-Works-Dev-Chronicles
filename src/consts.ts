70a337da7b2b1fccdb171192a17523d0cffbf147
马师傅 <13754059+master-mashifu1@user.noreply.gitee.com>
2024-09-25 00:25:48 +0800

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,108 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Site title and description
+export const SITE_TAB = "M-Works Dev Journal";
+export const SITE_TITLE = "Frosti 🧊";
+export const SITE_DESCRIPTION = "A blog template";
+export const DATE_FORMAT = "ddd MMM DD YYYY";
+
+// User profile information
+export const USER_NAME = "M-Works Dev Chronicles";
+export const USER_SITE = "https://github.com/Gnomeshgh19LvU"
+export const USER_AVATAR = "/profile.webp";
+
+// Server and transition settings
+export const SERVER_URL = "https://demo.saroprock.com";
+
+// Menu items for navigation
+export const menuItems = [
+  { id: "home", text: "首页", href: "/", svg: "home", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "about", target: "_self" }, // About page
+  {
+    id: "blog",
+    text: "博客",
+    href: "/blog",
+    svg: "blog",
+    target: "_self",
+    subItems: [
+      {
+        id: "all",
+        text: "所有博客",
+        href: "/blog",
+        svg: "post",
+        target: "_self",
+      }, // All blog
+      {
+        id: "tech",
+        text: "技术博客",
+        href: "/blog/categories/tech",
+        svg: "cube",
+        target: "_self",
+      }, // Technology category
+      {
+        id: "life",
+        text: "生活博客",
+        href: "/blog/categories/life",
+        svg: "heart",
+        target: "_self",
+      }, // Life category
+      {
+        id: "All categories",
+        text: "所有分类",
+        href: "/blog/categories/categories",
+        svg: "categories",
+        target: "_self",
+      }, // All categories
+    ],
+  }, // Blog page with sub-items
+  {
+    id: "project",
+    text: "项目",
+    href: "/project",
+    svg: "project",
+    target: "_self",
+  }, // Projects page
+  {
+    id: "friend",
+    text: "朋友",
+    href: "/friend",
+    svg: "friend",
+    target: "_self",
+  }, // Friends page
+  {
+    id: "contact",
+    text: "联系",
+    href: "mailto:contact.evesunmaple@outlook.com", // Contact email
+    target: "_blank", // Open in a new tab
+    svg: "contact",
+  },
+];
+
+// Social media and contact icons
+export const socialIcons = [
+  {
+    href: "https://afdian.net/a/saroprock",
+    ariaLabel: "Support my work",
+    title: "Support my work",
+    svg: "support",
+  },
+  {
+    href: "https://github.com/Gnomeshgh19LvU",
+    ariaLabel: "Github",
+    title: "Github",
+    svg: "github",
+  },
+  {
+    href: "https://space.bilibili.com/1796832871",
+    ariaLabel: "BiliBili",
+    title: "BiliBili",
+    svg: "bilibili",
+  },
+  {
+    href: "/rss.xml",
+    ariaLabel: "RSS Feed",
+    title: "RSS Feed",
+    svg: "rss",
+  },
+];
